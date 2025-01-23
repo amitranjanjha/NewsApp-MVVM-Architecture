bb21ff7e6d4f3d2d45f7c176bfb85679b9635637
Amit Ranjan <Amit.Jha1@gds.ey.com>
2025-01-24 01:55:03 +0530

    Setup project

---
+++ app/src/main/java/com/amit/newsapp_mvvm_architechture/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amit.newsapp_mvvm_architechture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
